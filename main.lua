3f5176ed2d7530d9d95062920dac766320881fd5
nightfox133 <mar2385@columbia.edu>
2025-01-19 16:25:46 -0800

    modified:   main.lua 	new file:   player.lua

--- main.lua
+++ main.lua
@@ -1,63 +1,33 @@
+local wf = require "libraries/windfield"
+local camera = require 'libraries/camera'
+local anim8 = require 'libraries/anim8'
+local sti = require 'libraries/sti'
+local toggle = require "toggle"
+local Player = require 'player'  -- Require the Player module
+
 function love.load()
-    -- import
+    -- Initialize toggle variables
+    gameMusic = toggle:new()
+    test = toggle:new()
 
-    --init toggle vars--
-    toggle = require "toggle" 
-      gameMusic = toggle:new()
-      test = toggle:new()
-
-
-    wf = require "libraries/windfield"
+    -- Setup world and camera
     world = wf.newWorld(0, 0)
-
-
-    camera = require 'libraries/camera'
     cam = camera()
-    
-    anim8 = require 'libraries/anim8'
-    
-    sti = require 'libraries/sti'
+
+    -- Load game map and background
     gameMap = sti('maps/36x36.lua')
-    
-    --un blurry
-    love.graphics.setDefaultFilter("nearest", "nearest")
-
-
-
-
-    gs = .2 -- frame length ().2)
-
-    player = {} -- init 
-    -------atrr-------
-        player.x = 1000
-        player.y = 450
-        
-        --speed--
-        player.walkSpeed = 200 --default (200)
-        player.sprintSpeed = player.walkSpeed * 1.8 -- (1.8)
-        player.speed = player.walkSpeed -- set
-
-    --sprites--
-    player.spriteSheet = love.graphics.newImage('sprites/simplePlayerSheet.png')
-
     background = love.graphics.newImage('weirdTown.png')
 
-    -- red hood is (19, 21)
-    player.grid = anim8.newGrid(64, 63, player.spriteSheet:getWidth(), player.spriteSheet:getHeight(), 0 , 10) --geuss and check
+    -- Set graphics filter
+    love.graphics.setDefaultFilter("nearest", "nearest")
 
-    player.animations = {} 
-    -- default animations                              --number row time
-    player.animations.up = anim8.newAnimation(player.grid("1-4", 4), gs) -- (.2 || .1)
-    player.animations.down = anim8.newAnimation(player.grid("1-4", 1), gs)
-    player.animations.left = anim8.newAnimation(player.grid("1-4", 2), gs)
-    player.animations.right = anim8.newAnimation(player.grid("1-4", 3), gs) -- idle 6
-    ---------------------------------------------------------------------
+    -- Frame duration (0.2 seconds per frame)
+    gs = 0.2
 
-    player.anim = player.animations.left
+    -- Load player and initialize
+    Player.load(world)  -- Load the player with the world passed in
 
-    player.collider = world:newBSGRectangleCollider(player.x, player.y, 38, 53, 12) --(38,53,12)
-    player.collider:setFixedRotation(true)
-
+    -- Initialize walls from the map
     walls = {}
     if gameMap.layers["walls"] then 
         for i, obj in pairs(gameMap.layers["walls"].objects) do
@@ -67,155 +37,98 @@ function love.load()
         end
     end
 
-    --sounds--
-        -- sound fx: static
-        -- muisic: stream
+    -- Sound setup
     sounds = {}
     sounds.blip = love.audio.newSource("sounds/blip.mp3", "static")
     sounds.music = love.audio.newSource("sounds/8bit-music-test.mp3", "stream")
-
     sounds.music:play()
     sounds.music:setLooping(true)
 end
 
 function love.update(dt)
-    local isMoving = false
-    
-    -- CONTROLS --
+    -- Update the player with the delta time
+    Player.update(dt)
 
-    local vx = 0
-    local vy = 0
+    -- Camera follow player
+    cam.x, cam.y = Player.x, Player.y
 
-    -- right arrow key
-    if love.keyboard.isDown("right") then
-        vx = player.speed
-        player.anim = player.animations.right
-        isMoving = true 
-    end
-
-    -- left arrow key
-    if love.keyboard.isDown("left") then
-        vx =  player.speed * -1
-        player.anim = player.animations.left
-        isMoving = true
-    end
-
-    -- up arrow key
-    if love.keyboard.isDown("up") then
-        vy =  player.speed * -1
-        player.anim = player.animations.up
-        isMoving = true
-    end
-
-    -- down arrow key
-    if love.keyboard.isDown("down") then
-        vy =  player.speed
-        player.anim = player.animations.down
-        isMoving = true
-    end
-
-    -- shift key
-    if love.keyboard.isDown("lshift") or love.keyboard.isDown("rshift") then
-        player.speed = player.sprintSpeed
-    else
-        player.speed = player.walkSpeed
-    end
-
-    -----update-------
-
-    player.collider:setLinearVelocity(vx, vy)
-
-
-    if isMoving == false then
-        player.anim:gotoFrame(1) --goes to standing still frame
-    end
-
-
-    player.anim:update(dt) -- update
-    
-    world:update(dt)
-    player.x = player.collider:getX() - 32
-    player.y = player.collider:getY() - 30
-
-
-    cam.x, cam.y = player.x, player.y
     local mapw = background:getWidth()
-    local maph = background:getHeight()  
-
+    local maph = background:getHeight()
     local w = love.graphics.getWidth()
     local h = love.graphics.getHeight()
 
-    -- left bound
-    if cam.x < w/2 then 
-        cam.x = w/2
+    -- Camera boundaries
+    if cam.x < w / 2 then
+        cam.x = w / 2
     end
-    -- right bound
-    if cam.x > mapw + w/2 then 
-        cam.x = mapw + w/2
+    if cam.x > mapw + w / 2 then
+        cam.x = mapw + w / 2
     end
-    -- up bound
-    if cam.y < h/2 then
-        cam.y = h/2
+    if cam.y < h / 2 then
+        cam.y = h / 2
     end
-    -- bottom bound
-    if cam.y > maph*2 + h then 
-        cam.y = maph*2 + h
+    if cam.y > maph * 2 + h then
+        cam.y = maph * 2 + h
     end
 
+    cam:lookAt(cam.x, cam.y)
 
-    cam:lookAt(cam.x, cam.y) --follow player
-
+    world:update(dt)  -- Update world (for collision detection)
 end
-
 
 function love.draw()
     local xBG, yBG = 3
     local xP, yP = 1, 1
-    love.graphics.setColor(1, 1, 1) -- set default
+    love.graphics.setColor(1, 1, 1) -- Set default color
 
     cam:attach()
-        gameMap:drawLayer(gameMap.layers["walls"])
-    
-        love.graphics.draw(background, 0, 0, 0, xBG, yBG) --drawn first (back layer)
 
-        
-        --gameMap:drawLayer(gameMap.layers["ground"]) --(major distortion)
+    -- Draw the game map and walls
+    gameMap:drawLayer(gameMap.layers["walls"])
 
+    -- Draw background
+    love.graphics.draw(background, 0, 0, 0, xBG, yBG)
 
-        player.anim:draw(player.spriteSheet, player.x, player.y, 0, xP, yP)
-        world:draw()
+    -- Draw player animation
+    Player.draw()  -- Draw the player using the Player module's draw function
+
+    -- Draw world colliders
+    world:draw()
+
     cam:detach()
-    --text--
-    love.graphics.setFont(love.graphics.newFont(20)) -- Sets the default font with size 14
-    love.graphics.setColor(0, 0, 0) --text color
-    love.graphics.print("Player: (" .. player.x .. ", " .. player.y .. ")", 0, 0) -- player location
 
+    -- Display player coordinates
+    love.graphics.setFont(love.graphics.newFont(20))
+    love.graphics.setColor(0, 0, 0) -- Text color
+    love.graphics.print("Player: (" .. Player.x .. ", " .. Player.y .. ")", 0, 0)
 
-
-    if test:get() then
-        love.graphics.print("The toggle is ON", 400, 300)
+    -- Toggle message
+    if gameMusic:get() then
+        love.graphics.print("music: OFF  'm' to toggle", 0, 20)
     else
-        love.graphics.print("The toggle is OFF", 400, 300)
+        love.graphics.print("music: ON   'm' to toggle", 0, 20)
     end
 end
 
 function love.keypressed(key)
-    -- teloport
+    -- Teleport on space key press
     if key == "space" then
-        player.collider:setX(880)
-        player.collider:setY(600)
+        Player.collider:setX(880)
+        Player.collider:setY(600)
     end
-    -- attack
+
+    -- Attack on 'a' key press
     if key == "a" then
         love.graphics.print("attack", 0, 0)
         sounds.blip:play()
     end
-    
+
+    -- Toggle music on 'm' key press
     if key == "m" then
         gameMusic:toggle()
         if gameMusic:get() then
             sounds.music:pause()
-        else 
+        else
             sounds.music:play()
         end
     end
---
+++ player.lua
@@ -0,0 +1,77 @@
+-- player.lua
+
+local anim8 = require 'libraries/anim8'
+
+local Player = {}
+Player.x = 1000
+Player.y = 450
+Player.walkSpeed = 200
+Player.sprintSpeed = Player.walkSpeed * 1.8
+Player.speed = Player.walkSpeed
+Player.spriteSheet = love.graphics.newImage('sprites/simplePlayerSheet.png')
+Player.grid = anim8.newGrid(64, 63, Player.spriteSheet:getWidth(), Player.spriteSheet:getHeight(), 0, 10)
+
+Player.animations = {
+    up = anim8.newAnimation(Player.grid("1-4", 4), 0.2),
+    down = anim8.newAnimation(Player.grid("1-4", 1), 0.2),
+    left = anim8.newAnimation(Player.grid("1-4", 2), 0.2),
+    right = anim8.newAnimation(Player.grid("1-4", 3), 0.2),
+}
+
+Player.anim = Player.animations.left
+
+function Player.load(world)
+    -- Collider initialization
+    Player.collider = world:newBSGRectangleCollider(Player.x, Player.y, 38, 53, 12)
+    Player.collider:setFixedRotation(true)
+end
+
+function Player.update(dt)
+    local isMoving = false
+    local vx, vy = 0, 0
+
+    -- Update position based on input
+    if love.keyboard.isDown("right") then
+        vx = Player.speed
+        Player.anim = Player.animations.right
+        isMoving = true
+    end
+    if love.keyboard.isDown("left") then
+        vx = -Player.speed
+        Player.anim = Player.animations.left
+        isMoving = true
+    end
+    if love.keyboard.isDown("up") then
+        vy = -Player.speed
+        Player.anim = Player.animations.up
+        isMoving = true
+    end
+    if love.keyboard.isDown("down") then
+        vy = Player.speed
+        Player.anim = Player.animations.down
+        isMoving = true
+    end
+
+    -- Sprinting
+    if love.keyboard.isDown("lshift") or love.keyboard.isDown("rshift") then
+        Player.speed = Player.sprintSpeed
+    else
+        Player.speed = Player.walkSpeed
+    end
+
+    Player.collider:setLinearVelocity(vx, vy)
+
+    if not isMoving then
+        Player.anim:gotoFrame(1) -- Go to the idle frame
+    end
+
+    Player.anim:update(dt)
+    Player.x = Player.collider:getX() - 32
+    Player.y = Player.collider:getY() - 30
+end
+
+function Player.draw()
+    Player.anim:draw(Player.spriteSheet, Player.x, Player.y)
+end
+
+return Player
